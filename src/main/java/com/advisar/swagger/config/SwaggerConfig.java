4e856483cf6b1d23228e47128ab0ebcaeb65905a
010100100000 <treece@masonite.com>
2018-11-30 15:22:02 -0500

    Added SwaggerConfig and made unneccessary configurations unused

---
+++ src/main/java/com/advisar/swagger/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package com.advisar.swagger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/*
+    Copy and paste this configuration
+    inside whatever application you want to
+    have API documentation for and uncomment the annotations.
+    This file in not necessary inside this application.
+ */
+//@Configuration
+//@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Auth REST API")
+                .description("\"Developer's Rest API\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Tyler Reece", "", "treece@masonite.com"))
+                .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
